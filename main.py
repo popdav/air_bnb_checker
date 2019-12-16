1c732ed21685c60495e31b48c7154233b53e2d53
popdav <popdav@outlook.com>
2019-12-16 22:39:48 +0100

    added multi search

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 import json
 
-f = open("/home/popdav/rista/airbnb_checker/scrapers/conf.json", "r")
+f = open("./scrapers/conf.json", "r")
 data = f.read()
 dataJson = json.loads(data)
 f.close()
