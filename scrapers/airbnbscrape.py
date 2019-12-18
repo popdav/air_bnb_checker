3d032fcda830097df58f78887f9de2ec401c89c3
popdav <popdav@outlook.com>
2019-12-18 10:02:23 +0100

    edited file name

--- scrapers/airbnbscrape.py
+++ scrapers/airbnbscrape.py
@@ -10,54 +10,40 @@ class AirbnbSpider(scrapy.Spider):
 
     def __init__(self, dataJson, **kwargs):
         super().__init__(**kwargs)
+        try:
+            if dataJson is None:
+                raise Exception("Conf file error, json object is None")
 
-        if dataJson is None:
-            print("Conf file error, json object is None")
-            sys.exit(1)
+            now_date = datetime.now()
+            checkin = datetime.fromisoformat(dataJson['checkin'])
+            checkout = datetime.fromisoformat(dataJson['checkout'])
 
-        # f = open("/home/popdav/rista/airbnb_checker/scrapers/conf.json", "r")
-        # data = f.read()
-        # dataJson = json.loads(data)
-        # f.close()
-        now_date = datetime.now()
-        checkin = datetime.fromisoformat(dataJson['checkin'])
-        checkout = datetime.fromisoformat(dataJson['checkout'])
+            check_date_in_out = checkout < checkin
+            check_date_to_now = checkin < now_date or checkout < now_date
 
-        check_date_in_out = checkout < checkin
-        check_date_to_now = checkin < now_date or checkout < now_date
+            if check_date_in_out or check_date_to_now:
+                raise Exception("Wrong date")
 
-        if check_date_in_out or check_date_to_now:
-            print("Wrong date")
-            sys.exit(1)
+            url = 'https://www.airbnb.com/s/{}--{}/homes?refinement_paths%5B%5D=%2Fhomes&checkin={}&checkout={' \
+                  '}&adults={}&children={}&infants={}&search_type=pagination '
+            self.i = 2
 
-        url = 'https://www.airbnb.com/s/{}--{}/homes?refinement_paths%5B%5D=%2Fhomes&checkin={}&checkout={}&adults={' \
-              '}&children={}&infants={}&search_type=pagination'
-        self.i = 2
+            self.start_urls = [
+                url.format(dataJson['place'], dataJson['country'], dataJson['checkin'], dataJson['checkout'],
+                           dataJson['adults'], dataJson['children'], dataJson['infants'])
+            ]
 
-        self.start_urls = [
-            url.format(dataJson['place'], dataJson['country'], dataJson['checkin'], dataJson['checkout'],
-                       dataJson['adults'], dataJson['children'], dataJson['infants'])
-        ]
-
-        date_time_obj = datetime.now()
-        timestamp_str = date_time_obj.strftime("%d-%b-%Y_(%H:%M:%S.%f)")
-        self.file_name = 'place={}&country={}&checkin={}&checkout={}&adults={}&children={}&infants={}_{}.csv'.format(
-            dataJson['place'],
-            dataJson['country'], dataJson['checkin'], dataJson['checkout'],
-            dataJson['adults'], dataJson['children'], dataJson['infants'], timestamp_str)
-        t = open(self.file_name, "w+")
-        t.write('property_id,type,link\n')
-        t.close()
-
-        self.file_name_num_page = 'page_num_' + self.file_name
-        t = open(self.file_name_num_page, "w+")
-        t.write('property_id,page_number\n')
-        t.close()
+            date_time_obj = datetime.now()
+            timestamp_str = date_time_obj.strftime("%d-%b-%Y_(%H:%M:%S.%f)")
+            self.file_name = '{}_{}_{}_{}_{}.csv'.format(
+                dataJson['checkin'], dataJson['checkout'],  dataJson['place'], dataJson['country'], timestamp_str)
+            t = open(self.file_name, "w+")
+            t.write('property_id,page_number,type,link\n')
+            t.close()
+        except Exception as e:
+            print(e)
 
     def parse(self, response):
-        print('*******************************************************************************************************')
-        print(response.url)
-        print('*******************************************************************************************************')
         self.parse_page(response)
         str_url = '//ul[contains(@data-id, \"SearchResultsPagination\")]/li[contains(@data-id, \"page-{0}\")]/a/@href'.format(
             str(self.i))
@@ -71,7 +57,8 @@ class AirbnbSpider(scrapy.Spider):
 
     def parse_page(self, response):
 
-        rooms = response.xpath('//div[contains(@itemprop, \"itemListElement\")]/meta[contains(@itemprop, \"url\")]/@content').getall()
+        rooms = response.xpath('//div[contains(@itemprop, \"itemListElement\")]/meta[contains(@itemprop, '
+                               '\"url\")]/@content').getall()
 
         rooms = list(
             map(lambda x: 'https://www.airbnb.com/' +
@@ -81,13 +68,10 @@ class AirbnbSpider(scrapy.Spider):
         )
 
         f = open(self.file_name, 'a')
-        t = open(self.file_name_num_page, 'a')
         for room in rooms:
             typeR = re.search('https://www.airbnb.com/([a-zA-Z]*)/([0-9]*)', room).group(1)
             idR = re.search('https://www.airbnb.com/([a-zA-Z]*)/([0-9]*)', room).group(2)
-            f.write(idR + ',' + typeR + ',' + room + '\n')
-            t.write(idR + ',' + str(self.i - 1) + '\n')
+            f.write(idR + ',' + str(self.i - 1) + ',' + typeR + ',' + room + '\n')
 
         f.close()
-        t.close()
 
